d6a07cff9d4ecf1bc49146ab565feeab99a26dd9
Ruan Gabriel <rugabrielsilva@gmail.com>
2026-07-13 16:23:05 -0300

    Added input validation to metric conversions

--- modules/sm/mt_cm.js
+++ modules/sm/mt_cm.js
@@ -1,7 +1,10 @@
-function converter(valor){
+function converter(valor) {
+
+    if (typeof valor !== "number") {
+        throw new TypeError("O valor deve ser um número.");
+    }
 
     return valor * 100;
-
 }
 
 module.exports = converter;
--- modules/sm/mt_km.js
+++ modules/sm/mt_km.js
@@ -1,7 +1,10 @@
-function converter(valor){
+function converter(valor) {
+
+    if (typeof valor !== "number") {
+        throw new TypeError("O valor deve ser um número.");
+    }
 
     return valor / 1000;
-
 }
 
 module.exports = converter;
