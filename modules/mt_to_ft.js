044bd3d9f9f35ecf0f3a10829e16065c55b04dbb
tachyonora@gmail <revanfds@gmail.com>
2026-07-12 21:27:44 -0300

    Finished js function that converts meters to feet

--- modules/mt_to_ft.js
+++ modules/mt_to_ft.js
@@ -1,1 +1,7 @@
+function mt_ft(mt) {
+    if (!Number.isFinite(mt)) return NaN;
+    if (mt === Infinity || mt === -Infinity) return mt;
 
+    const conversion_ratio = 3.28084;
+    return mt * conversion_ratio;
+}
