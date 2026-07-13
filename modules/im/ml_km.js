6a58a541cbbd40a33aeb07ef01155c589156b4aa
tachyonora@gmail <revanfds@gmail.com>
2026-07-12 22:08:20 -0300

    Corrected conversion rate on ml_km.js

--- modules/im/ml_km.js
+++ modules/im/ml_km.js
@@ -2,6 +2,6 @@ function ml_KM(ml) {
     if (ml === Infinity || ml === -Infinity) return ml;
     if (!Number.isFinite(ml)) return NaN;
 
-    const conversion_ratio = 3.28084;
+    const conversion_ratio = 1.609;
     return ml * conversion_ratio;
 }
